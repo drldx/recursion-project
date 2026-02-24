91d8976591c207d093d9bad277337b6ee3633608
drldx <168914904+drldx@users.noreply.github.com>
2026-02-25 00:16:14 +0530

    update: iterative fib loop conditional

--- fibs.js
+++ fibs.js
@@ -1,11 +1,8 @@
 function fibs(n) {
   let res = [];
   for (let i = 0; i < n; i++) {
-    if (i <= 0) {
-      res.push(0);
-    }
-    else if (i === 1) {
-      res.push(1)
+    if (i <= 1) {
+      res.push(i);
     }
     else {
       res.push((res[i - 1]) + (res[i - 2]))
